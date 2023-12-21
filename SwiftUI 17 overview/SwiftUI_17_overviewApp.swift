b8b26f27d24912f2e4a842056d6922ceaed49eee
Renat Kislov <keetsta@icloud.com>
2023-12-21 16:41:52 +0500

    Initial Commit

---
+++ SwiftUI 17 overview/SwiftUI_17_overviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_17_overviewApp.swift
+//  SwiftUI 17 overview
+//
+//  Created by keet on 21.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_17_overviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
